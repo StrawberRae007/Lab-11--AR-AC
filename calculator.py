0fea70ed49e592ce3deeab5a0fb16a60ef7dde0c
audbittt <audreyreid@ufl.edu>
2026-04-13 21:38:16 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -4,8 +4,23 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
-
+import math
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    if a==0:
+        raise ZeroDivisionError("a is 0, can't divide by zero")
+    return b/a
+def logarithm(a,b):
+    if a<=0 or a==1:
+        raise ValueError("The logarithm base of a cannot be negative, 0, or 1")
+    if b<=0:
+        raise ValueError("The logarithm value of b must be greater than 0.")
+    return math.log(b,a)
+def exponent(a,b):
+    return a**b
 
