c1e3d2b76f2c870d8a8cbe59fe74a21ffa7c0909
audbittt <audreyreid@ufl.edu>
2026-04-13 21:52:53 -0400

    modified calculator again

--- calculator.py
+++ calculator.py
@@ -13,11 +13,11 @@ def multiply(a,b):
     return a*b
 def divide(a,b):
     if a==0:
-        raise ZeroDivisionError("a is 0, can't divide by zero")
+        raise ZeroDivisionError("a is 0, can't divide by zero.")
     return b/a
 def logarithm(a,b):
     if a<=0 or a==1:
-        raise ValueError("The logarithm base of a cannot be negative, 0, or 1")
+        raise ValueError("The logarithm base of a cannot be negative, 0, or 1.")
     if b<=0:
         raise ValueError("The logarithm value of b must be greater than 0.")
     return math.log(b,a)
